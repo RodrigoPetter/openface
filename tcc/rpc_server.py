43e31dee4b1c1e159fa25c7ffa3de292ce789a25
rodrigo <guigo45@gmail.com>
2017-09-24 22:35:14 -0300

    Versao 1

--- tcc/rpc_server.py
+++ tcc/rpc_server.py
@@ -4,6 +4,7 @@ import datetime
 import time
 import subprocess
 import infer
+import os
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -19,24 +20,35 @@ server = SimpleXMLRPCServer((endereco, porta),
 server.register_introspection_functions()
 print("Servidor registrado na porta: "+endereco+":"+str(porta))
 
-# Register a function under a different name
+# SOMA - EXEMPLO
 def exemplo(x,y):
     return x + y
 server.register_function(exemplo)
 
-def treinar():
+# Salvar foto
+def salvarFoto(nome, imagem64):
+    pasta = "../treinamento/originais/"+nome+"/"
+    if not os.path.exists(pasta):
+        os.makedirs(pasta)
+    print("Imagem recebida. Iniciando escrita em: "+pasta)
+    name = pasta+str(len(os.listdir(pasta)))+".png"
+    with open(name, "wb") as f:
+        print("Imagem recebida. Iniciando escrita em: "+pasta)    
+        f.write(imagem64.decode('base64'))      
 
-    #raw_input("Treinamento 1")
-    amostra = "final"
-    gerarClassifier(amostra)
-    
-    with open("../treinamento_tcc/final/feature/classifier.pkl", "rb") as f:
+    return '{ "result": "Imagem de '+nome+' salva com sucesso." }'
+server.register_function(salvarFoto)
+
+# Treinar
+def treinar():
+    gerarClassifier()
+    with open("../treinamento/feature/classifier.pkl", "rb") as f:
         class1 = f.read().encode('base64')      
 
-    return '{ "result":[{"nome":"Classifier1", "arquivo":"'+class1+'"}] }'
+    return '{ "classifier":"'+class1+'" }'
 server.register_function(treinar)
 
-def descobrir(treinamento64, imagem64):
+def descobrir(classifier64, imagem64):
     print("Imagem recebida. Iniciando escrita...")
     with open("../temp/imagem_temporaria.png", "wb") as f:
 	    f.write(imagem64.decode('base64'))
@@ -44,7 +56,7 @@ def descobrir(treinamento64, imagem64):
     
     print("Classificador recebido. Iniciando escrita...")
     with open("../temp/classifier.pkl", "wb") as f:
-        f.write(treinamento64.decode('base64'))
+        f.write(classifier64.decode('base64'))
     print("Escrita concluida.")
     
     result = infer.infer("../temp/classifier.pkl", "../temp/imagem_temporaria.png", True)
@@ -52,14 +64,14 @@ def descobrir(treinamento64, imagem64):
     return result
 server.register_function(descobrir)
 
-def gerarClassifier(amostra):
+def gerarClassifier():
     workdir = "/root/openface/"
     
-    print("Iniciando treinamento: "+amostra+" Foto")
+    print("Iniciando treinamento")
     start = time.time()
     #raw_input("alinhamento")
     print(datetime.datetime.now().time())
-    bashCommand = "./util/align-dlib.py ./treinamento_tcc/"+amostra+"/originais/ align outerEyesAndNose ./treinamento_tcc/"+amostra+"/alinhadas/ --size 96"
+    bashCommand = "./util/align-dlib.py ./treinamento/originais/ align outerEyesAndNose ./treinamento/alinhadas/ --size 96"
     p = subprocess.Popen(bashCommand, shell=True, cwd=workdir)
     p.wait()
     print(datetime.datetime.now().time())
@@ -69,7 +81,7 @@ def gerarClassifier(amostra):
     #raw_input("representacao")
     print(datetime.datetime.now().time())         
     start = time.time()
-    bashCommand = "./batch-represent/main.lua -outDir ./treinamento_tcc/"+amostra+"/feature/ -data ./treinamento_tcc/"+amostra+"/alinhadas/"
+    bashCommand = "./batch-represent/main.lua -outDir ./treinamento/feature/ -data ./treinamento/alinhadas/"
     p = subprocess.Popen(bashCommand, shell=True, cwd=workdir)
     p.wait()
     print(datetime.datetime.now().time())
@@ -79,7 +91,7 @@ def gerarClassifier(amostra):
     #raw_input("classificador")
     print(datetime.datetime.now().time())
     start = time.time()
-    bashCommand = "./demos/classifier.py train ./treinamento_tcc/"+amostra+"/feature/"
+    bashCommand = "./demos/classifier.py train ./treinamento/feature/"
     p = subprocess.Popen(bashCommand, shell=True, cwd=workdir)
     p.wait()
     print(datetime.datetime.now().time())
